ba4c977a8ba3366a3b70e008c8cf4785a4b03d5a
fegelsamuel <fegelsamuel@gmail.com>
2025-07-01 15:29:26 -0500

    TODO: Fix Font Type

--- app.vue
+++ app.vue
@@ -1,6 +1,25 @@
-<template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+<template lang="pug">
+header(class="bg-white shadow")
+  div(class="container mx-auto flex items-center justify-between py-4 px-6")
+    // Logo
+    div(class="flex items-center space-x-2")
+      img(src="/assets/gradient_no_name.png" class="size-auto")
+      div(class="text-xl font-bold font-tektur") Vetnologies
+slot
 </template>
+
+<script setup lang="ts">
+console.log("reload happened")
+</script>
+
+
+<style lang="css">
+.tektur-200 { 
+  font-family: "Tektur", sans-serif;
+  font-optical-sizing: auto;
+  font-weight: auto;
+  font-style: normal;
+  font-variation-settings:
+    "wdth" 100;
+}
+</style>
---
+++ tailwind.config.ts
@@ -0,0 +1,17 @@
+export default {
+    content: [
+      './components/**/*.{vue,js}',
+      './layouts/**/*.vue',
+      './pages/**/*.vue',
+      './app.vue',
+      './plugins/**/*.{js,ts}',
+    ],
+    theme: {
+      extend: {
+        fontFamily: {
+            tektur: ['Tektur', 'sans-serif']
+        }
+      },
+    },
+    plugins: [],
+}
