3bd0930f5457b41db0a2e7c91abe06a3929f664c
FegelSamuel <fegelsamuel@gmail.com>
2025-07-06 15:11:42 -0500

    security done index2

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,24 @@
 <template lang="pug">
 div(class="flex flex-box grid-cols-2 gap-2 size-full mt-8")
-    h1(class="text-4xl text-bold ml-16 mt-32") Why would you need both Tech Support and IT Strategy?
-        div(class="text-xl font-sans text-black") While some veterinaries may know a guy who knows a guy who has some IT experience it may not be enough to navigate the complexity of business class IT. Just getting your printer to print again as a quick fix is not all that can happen on a computer, computer network, or server.
-    img(src="/assets/gradient.png" class="h-128 mr-32")
+    h1(class="text-4xl font-bold ml-16 mt-32") Why would you need both Tech Support and IT Strategy?
+        div(class="text-xl font-sans font-normal text-black") While some veterinaries may know a guy who knows a guy who has some IT experience it may not be enough to navigate the complexity of business class IT. Just getting your printer to print again as a quick fix is not all that can happen on a computer, computer network, or server.
+    img(src="/assets/gradient.png" class="lg:h-128 lg:mr-32")
 div(class="flex flex-box grid-cols-2 gap-2 size-full mt-16")
     img(src="/assets/caducus.png" class="h-100 mr-32")
-    h1(class="text-4xl text-bold ml-16 mt-16") Why Choose Vetnology for Your Veterinary IT Needs
+    h1(class="text-4xl ml-16 mt-16") Why Choose Vetnology for Your Veterinary IT Needs
         div(class="text-xl font-sans text-black") At Vetnology, we understand the unique challenges that veterinary clinics face in today’s fast-paced, digital world. Our IT solutions are specifically designed to help your practice operate more efficiently, protect sensitive patient data, and provide the best possible care for your clients and their pets.
-
+div(class="flex flex-box grid-cols-2 gap-2 size-full mt-16")
+    h1(class="text-4xl ml-16 mt-32") Expertise in Veterinary Practice Technology
+        div(class="text-xl font-sans text-black")
+            | We specialize in veterinary practice management software—such as Impromed, Cornerstone, and more—ensuring seamless integration and reliable support for the systems you rely on every day. Our team is experienced in troubleshooting and optimizing these platforms, so you can focus on what matters most: 
+            span(class="font-bold") your patients
+            | .
+    a(href="https://covetrus.com/" class="lg:h-128 lg:ml-16")
+        img(src="/assets/covetrusIcon.png")
+div(class="flex flex-box grid-cols-2 gap-2 size-full mt-16")
+    img(src="/assets/Cyber-Security-Cropped.png" class="lg:h-128 lg:ml-16")
+    h1(class="text-4xl mr-32 ml-16") Robust Security and Compliance
+        div(class="text-xl font-sans text-black") Veterinary practices handle sensitive patient and client information. We implement advanced security measures, including data encryption, secure mobile access, and compliance with industry regulations, to protect your practice from breaches and costly penalties.
 </template>
 
 <script setup lang="ts">
