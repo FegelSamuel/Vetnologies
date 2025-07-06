cc3a1733940697a4cd73930520c27e682e96a9e7
FegelSamuel <fegelsamuel@gmail.com>
2025-07-06 14:28:35 -0500

    index paragraph 2 done

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,11 @@ div(class="flex flex-box grid-cols-2 gap-2 size-full mt-8")
     h1(class="text-4xl text-bold ml-16 mt-32") Why would you need both Tech Support and IT Strategy?
         div(class="text-xl font-sans text-black") While some veterinaries may know a guy who knows a guy who has some IT experience it may not be enough to navigate the complexity of business class IT. Just getting your printer to print again as a quick fix is not all that can happen on a computer, computer network, or server.
     img(src="/assets/gradient.png" class="h-128 mr-32")
+div(class="flex flex-box grid-cols-2 gap-2 size-full mt-16")
+    img(src="/assets/caducus.png" class="h-100 mr-32")
+    h1(class="text-4xl text-bold ml-16 mt-16") Why Choose Vetnology for Your Veterinary IT Needs
+        div(class="text-xl font-sans text-black") At Vetnology, we understand the unique challenges that veterinary clinics face in today’s fast-paced, digital world. Our IT solutions are specifically designed to help your practice operate more efficiently, protect sensitive patient data, and provide the best possible care for your clients and their pets.
+
 </template>
 
 <script setup lang="ts">
